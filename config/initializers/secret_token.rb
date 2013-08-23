78a0707ce9184628567d1b11f4162c065b6655b5
Andrei Latchescu <andrei@nextbyte.ro>
2013-08-23 18:36:35 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DavisInfo::Application.config.secret_token = '4973cbd4706372b9438ce331d13f37725c0449ca4aa263c6b76e2f07c9afe896d52b8fc1904c63123d99b5f0d6d0d2edf3644df5ceb50919c98f3f05b8508347'
